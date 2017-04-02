84530aa3e731f26762700fe1d9fcf60060e7b9fc
Rayho <rayho360@gmail.com>
2017-04-02 23:25:58 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/coolweather/android/gson/Basic.java
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -13,7 +13,7 @@ public class Basic {
     @SerializedName("id")
     public String weatherId;
 
-    public Update updapte;
+    public Update update;
 
     public class Update{
         @SerializedName("loc")
